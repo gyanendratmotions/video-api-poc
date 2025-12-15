40c40c34bb0a309a7f14bc0a5933c7efa8f278b7
gyanendratmotions <gyanendra.singh@tmotions.com>
2025-12-15 10:54:52 +0530

    commit_test

--- backend/routes/api.py
+++ backend/routes/api.py
@@ -4,4 +4,5 @@ router = APIRouter()
 
 router.post("/get_transcript")(get_transcript)
 router.post("/transcript_improvement")(transcript_improvement)
+
 router.post("/generate_video")(generate_video)
